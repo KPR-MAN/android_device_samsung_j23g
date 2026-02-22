f023fd4cc8cfb1931f9299b22907131b1de3db85
K-MAX <158112978+KPR-MAN@users.noreply.github.com>
2026-02-23 01:34:09 +0200

    Update j23g.mk

--- j23g.mk
+++ j23g.mk
@@ -12,41 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/samsung/j23g
+# Inherit from TWRP common
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from vendor tree
-$(call inherit-product-if-exists, vendor/samsung/j23g/j23g-vendor.mk)
-
-# Inherit from scx30g2-common device configuration
-$(call inherit-product, device/samsung/scx30g2-common/common.mk)
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
-
-# Keylayouts
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
-
-# sdcardfs
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sys.sdcardfs=true
-
-# Offmode charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    cm_charger_res_images
-
-# Low-RAM configs
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.bg_apps_limit=10 \
-    config.disable_atlas=true 
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_j23g
+PRODUCT_NAME := twrp_j23g
 PRODUCT_DEVICE := j23g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SM-J200H
+
+PRODUCT_RELEASE_NAME := Galaxy J2
+
+# Boot animation resolution
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
